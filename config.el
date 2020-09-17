0edfedb0cac621140d270ebef6792c36e195ffe1
Nicholas Miklaucic <nicholas.miklaucic@gmail.com>
2020-09-17 13:37:07 -0400

    Changed fonts

--- config.el
+++ config.el
@@ -19,9 +19,9 @@
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 
-(setq doom-font (font-spec :family "Liga Plex Mono" :weight 'semibold :size 14))
-(setq doom-variable-pitch-font (font-spec :family "Recursive Sans Linear Static" :size 15))
-(setq doom-big-font (font-spec :family "Liga Plex Mono" :weight 'semibold :size 18))
+(setq doom-font (font-spec :family "LigaPlexMono Nerd Font" :weight 'semibold :size 14))
+(setq doom-variable-pitch-font (font-spec :family "IBM Plex Sans" :size 15))
+(setq doom-big-font (font-spec :family "LigaPlexMono Nerd Font" :weight 'semibold :size 18))
 
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
